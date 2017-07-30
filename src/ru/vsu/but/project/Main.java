586c0c790e2df019b3fa2334d96d3fb13775353f
suporelevel <a.boot96@gmail.com>
2017-07-30 14:40:19 +0300

    first program commit

---
+++ src/ru/vsu/but/project/Main.java
@@ -0,0 +1,10 @@
+package ru.vsu.but.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(123 + 123);
+        System.out.println("HEYA");// write your code here
+    }
+}
+
